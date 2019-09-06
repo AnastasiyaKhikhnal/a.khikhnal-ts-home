2a2f5e7e64f9e9bca46110a243c8cf410e974bba
Anastasiya Khikhnal <a.khikhnal@gmail.com>
2019-09-06 15:08:36 +0300

    Delete hello.component.ts from task1

--- lesson-6-angular-architecture/task1/src/app/app.module.ts
+++ lesson-6-angular-architecture/task1/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { WarningComponent } from './../app/warning-alert/warning-alert.component';
 import { SuccessComponent } from './../app/success-alert/success-alert.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, WarningComponent, SuccessComponent ],
+  declarations: [ AppComponent, WarningComponent, SuccessComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
